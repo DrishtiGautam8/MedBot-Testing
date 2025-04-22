14746185868314b6254eb408fbd86df638ed63ae
DrishtiGautam <drishtig2023version@gmail.com>
2025-04-22 13:45:04 +0530

    Update evaluate_queries.py

--- evaluate_queries.py
+++ evaluate_queries.py
@@ -39,8 +39,8 @@ def load_data():
     queries_sheet = gc.open_by_url(QUERIES_SHEET_URL)
     feedback_sheet = gc.open_by_url(FEEDBACK_SHEET_URL)
 
-    queries_df = pd.DataFrame(queries_sheet.sheet1.get_all_records())
-    feedback_df = pd.DataFrame(feedback_sheet.sheet1.get_all_records())
+    queries_worksheet = queries_sheet.get_worksheet(0)
+    feedback_worksheet = feedback_sheet.get_worksheet(0)
 
     queries_df = pd.DataFrame(queries_worksheet.get_all_records())
 
@@ -54,6 +54,7 @@ def load_data():
             "selected_best_model"])
 
     return queries_df, feedback_df
+
 
 
 # Get unrated grouped queries
